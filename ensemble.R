7feee2a72bc346305ee7e5605df6abed502c2986
Varun <varunpurohit76@gmail.com>
2016-02-29 07:10:30 +0530

    ensembled prediction

---
+++ ensemble.R
@@ -0,0 +1,5 @@
+one <- read.csv("C:/Users/Varun/Desktop/kaggle/mmlm/ensemble/0.376371.csv")
+two <- read.csv("C:/Users/Varun/Desktop/kaggle/mmlm/ensemble/0.376770.csv")
+three <- two
+three$Pred <- (one$Pred+two$Pred)/2
+write.csv(three,"C:/Users/Varun/Desktop/kaggle/mmlm/ensemble/predict.csv", row.names = FALSE)
